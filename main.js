2c175a7f5fc7d811157c15c8615bc1daee672d9a
JaoodxD <jaood96@gmail.com>
2023-10-08 14:42:50 +0300

    Fix bug with progressbar aways shows 0 progress

--- main.js
+++ main.js
@@ -1,41 +1,32 @@
 import Wallhaven from './lib/wallhaven.js'
 import LocalStorage from './lib/localStorage.js'
 import ProgressBar from 'progress'
+
 const storage = LocalStorage('Jaood')
-// const info = storage.addImage({
-//   id: 'test',
-//   colors: '#123',
-//   path: 'some random url',
-//   url: 'http://wallhaven.org'
-// })
 
 const API_KEY = process.env.WALLHAVEN_API_KEY
 const wh = Wallhaven(API_KEY, 'Jaood')
 
 const collections = await wh.userCollections()
 console.log(collections)
-
+console.time('total')
 for (const collection of collections) {
-  const { id } = collection
+  const { id, label, count } = collection
+  console.time(label)
   let currentPage = 0
   let lastPage = -1
+  const bar = new ProgressBar(`  ${label} [:bar] :rate/bps :percent :etas`, {
+    total: count
+  })
   do {
     currentPage++
     const { data, meta } = await wh.collectionInfo(id, currentPage)
     lastPage = meta.last_page
 
-    const bar = new ProgressBar(
-      '  downloading [:bar] :rate/bps :percent :etas',
-      {
-        total: collection.count
-      }
-    )
-    // console.log(meta)
     for (const image of data) {
       const { id, url, path, colors } = image
       const result = storage.addImage({ id, colors, path, url })
       const imageId = result.id
-      // console.log({ id, imageId })
       const tags = await wh.wpTags(id)
       for (const tag of tags) {
         const { id: tagId, name } = tag
@@ -45,6 +36,9 @@ for (const collection of collections) {
       bar.tick(1)
     }
   } while (currentPage !== lastPage)
+  bar.terminate()
+  console.timeEnd(label)
 }
+console.timeEnd('total')
 
 storage.close()
